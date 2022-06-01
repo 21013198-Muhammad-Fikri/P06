eb1c306ad8fc4bc827c0ea8fdf384635b913bcb8
21013198 <21013198@172.29.80.158>
2022-06-01 14:50:47 +0800

    Calculator

---
+++ P06/src/Calculate.java
@@ -0,0 +1,21 @@
+public class Calculate {		
+	public int add (int a, int b) {
+		return a +b;
+	}
+	public int substract (int a, int b) { 
+		  return a - b; 
+	} 
+	
+	public int multiply (int a, int b) { 
+		  return a * b; 
+		  } 
+	public int divide (int a, int b) { 
+		if (b == 0) {
+			throw new IllegalArgumentException("Division by zero is not supported");
+		}
+		return a/b; 
+		
+	}
+	 
+
+}
---
+++ P06/test/CalculateTest.java
@@ -0,0 +1,88 @@
+import static org.junit.Assert.*; 
+ 
+import org.junit.After; 
+import org.junit.AfterClass; 
+import org.junit.Before; 
+import org.junit.BeforeClass; 
+import org.junit.Test; 
+ 
+public class CalculateTest { 
+ 
+	private int a,b,c; 
+	Calculate cal; 
+  
+	@BeforeClass 
+	public static void setUpBeforeClass() throws Exception { 
+	
+	} 
+ 
+	@AfterClass 
+	public static void tearDownAfterClass() throws Exception { 
+ 
+	} 
+ 
+	@Before 
+	public final void setUp() throws Exception { 
+		a = 4321; 
+		b = 1234; 
+		c = 0;   
+		cal = new Calculate(); 
+ 
+ 
+	} 
+ 
+	@After 
+	public final void tearDown() throws Exception { 
+	} 
+ 
+	@Test 
+	public final void testAdd() { 
+		int actual = cal.add(a, b);  
+      
+		int expected = 5555; 
+		assertEquals (expected, actual); 
+ 
+	} 
+  
+	@Test 
+	public final void testSubtract() { 
+		int actual = cal.substract(a, b);  
+     
+		int expected = 3087; 
+		assertEquals (expected, actual); 
+	} 
+  
+	@Test 
+	public final void testMultiply() { 
+ 
+		int actual = cal.multiply(a, b);  
+     
+		int expected = 5332114; 
+		assertEquals (expected, actual); 
+ 
+	} 
+ 
+	@Test 
+	public final void testDivide() { 
+		int actual = cal.divide(a, b);  
+     
+		int expected = 3; 
+		assertEquals (expected, actual); 
+	} 
+  
+	@Test 
+	public final void testDivideBy0() { 
+		try { 
+			cal.divide(a,c); 
+			fail("Expected an IllegalArgumentException to be thrown"); 
+		} 
+		catch (IllegalArgumentException e){ 
+			assertEquals("Division by zero is not supported", e.getMessage()); 
+		} 
+		catch(Throwable t) { 
+			assertEquals("Expected an IllegalArgumentException to be thrown", t.getMessage()); 
+		} 
+	} 
+  
+ 
+}
